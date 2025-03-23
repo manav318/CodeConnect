64becba41e6d611b2e2c4074c090c4e791d8f553
Manav Ekka <itsmanav999@gmail.com>
2025-03-24 02:43:05 +0530

    auth/clerk setup

--- codeconnect/src/app/page.tsx
+++ codeconnect/src/app/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import {SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Sign in</Button>
+    <div className="m-10">
+      <SignedOut>
+          <SignInButton />
+      </SignedOut>
+      <SignedIn>
+          <UserButton />
+      </SignedIn>
     </div>
   );
 }
